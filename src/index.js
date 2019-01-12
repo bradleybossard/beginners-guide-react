82ae0cef1ff2362f9bfa774070ccd857c82a37e9
Bradley Bossard <bradleybossard@gmail.com>
2019-01-12 11:27:33 -0800

    Convert to using naive React calls

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "./styles.css";
+import React from 'react';
+import ReactDom from 'react-dom';
 
 const rootElement = document.getElementById("root");
+/*
 const element = document.createElement("div");
 
 element.textContent = "Hello World";
 element.className = "container";
 rootElement.appendChild(element);
+*/
+const element = React.createElement('div', {className: 'container'}, 'Hello World!');
+console.log(element);
+ReactDom.render(element, rootElement);
