268cb76cedd6edba7d706c21831ca4499a96816a
Bradley Bossard <bradleybossard@gmail.com>
2019-01-12 12:43:06 -0800

    Trying out propTypes, not working yet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import React from "react";
 import ReactDom from "react-dom";
+import PropTypes from "prop-types";
 
 const rootElement = document.getElementById("root");
 
@@ -11,12 +12,22 @@ const props = {
   children: content
 };
 
-const Message = props => <div>{props.children}</div>;
+const Message = props => (
+  <div>
+    {props.children}
+    <span>{props.testType}</span>
+  </div>
+);
+
+// This prop types does not seem to be working, need to revisit
+Message.propTypes = {
+  testType: PropTypes.string
+};
 
 //const element = <div {...props} />;
 const element = (
   <div className="container">
-    <Message>Hello World</Message>
+    <Message testType={1.2}>Hello World</Message>
     <Message>Goodbye World</Message>
   </div>
 );
