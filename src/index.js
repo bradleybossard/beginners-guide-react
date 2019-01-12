d5f9e4ee606e26bfb1254f02b6470cf4a0ce47d5
Bradley Bossard <bradleybossard@gmail.com>
2019-01-12 11:28:49 -0800

    Use JSX to render dom node

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import "./styles.css";
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 const rootElement = document.getElementById("root");
-/*
-const element = document.createElement("div");
 
-element.textContent = "Hello World";
-element.className = "container";
-rootElement.appendChild(element);
-*/
-const element = React.createElement('div', {className: 'container'}, 'Hello World!');
-console.log(element);
+const element = <div className>Hello World</div>;
 ReactDom.render(element, rootElement);
